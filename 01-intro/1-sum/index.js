6a4a9850d06df0608eb42950d53080677bbe6e3f
AGmyzov <agmyzov@yandex.ru>
2020-09-29 18:46:58 +0300

    реализована функция sum, которая суммирует два числа

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -5,5 +5,11 @@
  * @returns {number}
  */
 export default function sum(m, n) {
-
+  if (m===undefined) {
+    return m;
+  } else if (n===undefined) {
+    return n;
+  } else {
+    return m + n;
+  }
 }
