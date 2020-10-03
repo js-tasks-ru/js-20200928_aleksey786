2ab67c6df8645c423672ece6ec27be1b36e7cf41
AGmyzov <agmyzov@yandex.ru>
2020-10-03 21:33:52 +0300

    оптимизация sortStrings по результатам кодеревью

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,10 +6,13 @@
  */
 
 export function sortStrings(arr, param = 'asc') {
-  let sortarr = arr.slice(0, arr.length);
-  sortarr.sort((a, b) => a.localeCompare(b, ["ru","en"], {sensitivity: "case", caseFirst: "upper"}));
-  if (param === 'desc') {
-    sortarr.reverse();
+  const sortarr = [...arr];
+  const LOCALES = ["ru","en"];
+  const COMPARE_OPT = {sensitivity: "case", caseFirst: "upper"};
+  if (param === 'asc') {
+    sortarr.sort((a, b) => a.localeCompare(b, LOCALES, COMPARE_OPT));
+  } else {
+    sortarr.sort((a, b) => b.localeCompare(a, LOCALES, COMPARE_OPT));
   }
   return sortarr;
 }
