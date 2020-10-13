de6d12cd9e9edceadea142d69f7c7ccd3587a794
AGmyzov <agmyzov@yandex.ru>
2020-10-13 21:59:15 +0300

    реализация NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,48 @@
 export default class NotificationMessage {
+  element; // HTMLElement;
 
+  constructor(message = '',
+              {duration = 1000,
+               type = 'success',
+              } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.progressValue = duration / 1000;
+    this.type = type;
+    this.render();
+    this.initEventListeners();
+  }
+
+  initEventListeners() {}
+
+
+  render() {
+    const div = document.createElement('div');
+    div.innerHTML = this.template();
+    this.element = div.firstElementChild;
+  }
+
+  template () {
+    return `<div class="notification ${this.type}" style="--value:${this.progressValue}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">Notification</div>
+                    <div class="notification-body">${this.message}</div>
+                </div>
+            </div>`;
+  }
+
+  show(target = document.body) {
+    clearTimeout(this.timerId);
+    target.append(this.element);
+    this.timerId = setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
