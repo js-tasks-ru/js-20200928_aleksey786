b04a146d10d5680b33a92bb4e529b4fdaed3b36d
AGmyzov <agmyzov@yandex.ru>
2020-10-28 19:53:23 +0300

    реализация ProductForm v1, SortableList

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,107 @@
 export default class SortableList {
+  element; // HTMLElement;
 
+  constructor({items = []} = {}) {
+    this.items = items;
+    this.render();
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    this.element.addEventListener('pointerdown', this.onMouseDown);
+  }
+
+  onMouseDown= (event) => {
+
+    if (event.target.dataset.deleteHandle!==undefined) {
+      event.target.closest('.sortable-list__item').remove();
+      return;
+    }
+
+    this.currentDroppable = event.target;
+
+    this.createPlaceHolder(this.currentDroppable);
+
+    this.prepareDroppable();
+
+    document.addEventListener('mousemove', this.onMouseMove);
+    document.addEventListener('mouseup', this.onMouseUp);
+  }
+
+  prepareDroppable () {
+    this.currentDroppable.style.width = this.currentDroppable.offsetWidth + 'px';
+    this.currentDroppable.style.height = this.currentDroppable.offsetHeight + 'px';
+    this.currentDroppable.style.left = this.currentDroppable.offsetLeft + 'px';
+    this.currentDroppable.style.top = this.currentDroppable.offsetTop + 'px';
+    this.currentDroppable.classList.add("sortable-list__item_dragging");
+    this.element.append(this.currentDroppable);
+  }
+
+  createPlaceHolder (element) {
+    this.placeholder = document.createElement('div');
+    this.placeholder.className = "sortable-list__placeholder";
+    this.placeholder.style.width = this.currentDroppable.offsetWidth + 'px';
+    this.placeholder.style.height = this.currentDroppable.offsetHeight + 'px';
+    element.before(this.placeholder);
+  }
+
+  moveAt(pageX, pageY) {
+    this.currentDroppable.style.left = pageX + 'px';
+    this.currentDroppable.style.top = pageY + 'px';
+  }
+
+  onMouseMove = (event) => {
+    this.moveAt(event.pageX, event.pageY);
+
+    const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    if (!elemBelow) return;
+
+    const droppableBelow = elemBelow.closest('.sortable-list__item');
+    if (!droppableBelow) return;
+
+    if (this.placeholder !== droppableBelow) {
+      if (droppableBelow.offsetTop > this.placeholder.offsetTop) {
+        this.placeholder.before(droppableBelow);
+      } else {
+        this.placeholder.after(droppableBelow);
+      }
+    }
+  }
+
+  onMouseUp = (event) => {
+    this.placeholder.after(this.currentDroppable);
+    this.placeholder.remove();
+    this.currentDroppable.classList.remove("sortable-list__item_dragging");
+    this.currentDroppable.style = '';
+
+    document.removeEventListener('mousemove', this.onMouseMove);
+    document.removeEventListener('mouseup', this.onMouseUp);
+  }
+
+
+  render() {
+    const div = document.createElement('div');
+    div.className = "sortable-list";
+
+    const items = this.items.map(item => {
+        const element = document.createElement('div');
+        element.className = "sortable-list__item";
+        element.setAttribute("data-grab-handle",'')
+        element.innerHTML = `<span>${item.outerHTML}</span>
+                             <span data-delete-handle>remove</span>`;
+        return element;
+    })
+
+    div.append(...items);
+    this.element = div;
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    document.removeEventListener('pointerdown', this.onMouseDown);
+  }
 }
